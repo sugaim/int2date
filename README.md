839ca6ef09ef136b9d512c1147598534cc5a8dbe
sugaim <50139852+sugaim@users.noreply.github.com>
2021-07-24 04:35:12 +0900

    update

--- README.md
+++ README.md
@@ -1,8 +1,11 @@
 # int2date README
-usage1: Type "Int2Date: Convert..." and input an integer(2~99999).
-usage2: Hover the mouse over an integer(2~99999).
-
 ## Features
-This just converts serial (excel) date into a date.
+This extension provides two functionalities.  
 
-### 1.0.0
+### Converter  
+On command palette, convert a serial (excel) date into a date.  
+
+### Hover  
+On a text editor, Int2Date provides a hover showing dates on serial (excel) dates.  
+The hover can be activated with `Int2Date: Activate Int2Date hover on current language`.  
+To deactivate hovers, please use `Int2Date: Deactivate all Int2Date hovers`.  
--- src/extension.ts
+++ src/extension.ts
@@ -21,38 +21,20 @@ function dateToStr(date: Date): string {
 	return date.getFullYear() + '/' + date.getMonth() + '/' + date.getDate();
 }
 
-
-// -----------------------------------------------------------------------------
-// actiavter
-//
-export function activate(context: vscode.ExtensionContext) {
+class Int2DateHoverProvider {
+	readonly langId: string;
+	active: boolean;
 	
-	console.log('Congratulations, your extension "int2date" is now active!');
+	constructor(langId: string) {
+		this.langId = langId;
+		this.active = true;
+	}
 
-	//
-	// command[int2date.convert]: convert a serial date into a date.
-	//
-	let convertCommand = vscode.commands.registerCommand('int2date.convert', () => {
-		vscode.window.showInputBox({
-			prompt: 'serialDate',
-			validateInput: param => {
-				return (param.match(/^\d{1,5}$/) !== null && 1 < +param)
-					? undefined
-					: "input: serial date (2~99999)"; 
-			}
-		}).then((value: string|undefined) => {
-			if (value === undefined) { return; }
-			let date = serialDateToDate(+value);
-			if (date === undefined) { return; }
-			vscode.window.showInformationMessage('result: ' + dateToStr(date));
-		});
-	});
-	context.subscriptions.push(convertCommand);
+	provideHover(doc: vscode.TextDocument, pos: vscode.Position) {
+		if (!this.active) {
+			return Promise.reject("This ");
+		}
 
-	//
-	// hover[xml]: hover converted result
-	//
-	let convertHover = {provideHover(doc: vscode.TextDocument, pos: vscode.Position) {
 		// extract an integer
 		let targetRange = doc.getWordRangeAtPosition(pos, /\d{1,5}/);		
 		if (targetRange === undefined) {
@@ -70,19 +52,168 @@ export function activate(context: vscode.ExtensionContext) {
 			targetRange.start.character, 
 			targetRange.end.character
 		);
+		vscode.extensions.getExtension
 		let date = serialDateToDate(serialDate);
-		if (date === undefined) { return Promise.reject("Fail to parse " + serialDate + " as a serial date."); }
+		if (date === undefined) {
+			return Promise.reject("Fail to parse " + serialDate + " as a serial date."); 
+		}
 
 		// return value
-		return Promise.resolve(new vscode.Hover('as date: ' + dateToStr(date)));
-	}};
-	context.subscriptions.push(vscode.languages.registerHoverProvider(
-		{language: 'xml'}, 
-		convertHover
-	));
+		return Promise.resolve(
+			new vscode.Hover(serialDate + ' as date: ' + dateToStr(date))
+		);
+	}
+}
+
+class Int2DataHoverProviderManager {
+	providers: Array<Int2DateHoverProvider>;
+
+	constructor() {
+		this.providers = [];
+	}
+
+	providerIndexFor(langId: string): number {
+		for (var i=0; i<this.providers.length; ++i) {
+			if (this.providers[i].langId === langId) {
+				return i;
+			}
+		}
+		return -1;
+	}
+
+	isActiveLang(langId: string): boolean {
+		let idx = this.providerIndexFor(langId);
+		if (idx < 0) {
+			return false;
+		}
+		return this.providers[idx].active;
+	}
+
+	dispose() {
+		for (var i=0; i<this.providers.length; ++i) {
+			this.providers[i].active = false;
+		}
+		this.providers.splice(0);
+	}
+}
+
+function getDataHoverProviderManager(context: vscode.ExtensionContext):
+	Int2DataHoverProviderManager
+{
+	var manager = undefined;
+	for (var i=0; i<context.subscriptions.length; ++i) {
+		if (context.subscriptions[i] instanceof Int2DataHoverProviderManager) {
+			manager = context.subscriptions[i] as Int2DataHoverProviderManager;
+			break;
+		}
+	}
+	if (manager === undefined) {
+		// no manager is found in context.subscriptions
+		manager = new Int2DataHoverProviderManager();
+		context.subscriptions.push(manager);
+	}
+	return manager;
+}
+
+
+// -----------------------------------------------------------------------------
+// activater
+//
+export function activate(context: vscode.ExtensionContext) {
+	var manager = getDataHoverProviderManager(context);
+	//
+	// command[int2date.convert]: convert a serial date into a date.
+	//
+	let convertCommand = vscode.commands.registerCommand('int2date.convert', () => {
+		vscode.window.showInputBox({
+			prompt: 'serialDate',
+			validateInput: param => {
+				return (param.match(/^\d{1,5}$/) !== null && 1 < +param)
+					? undefined
+					: "input: serial date (2~99999)"; 
+			}
+		}).then((value: string|undefined) => {
+			if (value === undefined) {
+				return;
+			}
+			let date = serialDateToDate(+value);
+			if (date === undefined) {
+				return;
+			}
+			vscode.window.showInformationMessage('result: ' + dateToStr(date));
+		});
+	});
+	context.subscriptions.push(convertCommand);
+
+	//
+	// command[int2date.activateHover]: activate Int2DateHoverProvider on current lang.
+	//
+	let activateHover = vscode.commands.registerCommand('int2date.activateHover', () => {
+		let langId = vscode.window.activeTextEditor?.document.languageId;
+		if (langId === undefined) {
+			return;
+		}
+
+		// check if it is already registered or not.
+		if (manager.isActiveLang(langId)) {
+			vscode.window.showInformationMessage('Int2Data: Hover is already activated on ' + langId);
+			return;
+		}
+
+		// register
+		let providerIdx = manager.providerIndexFor(langId);
+		if (providerIdx < 0) {
+			// provider is not managed by the manager. construct newly and register it.
+			let provider = new Int2DateHoverProvider(langId);
+			manager.providers.push(provider);
+			context.subscriptions.push(vscode.languages.registerHoverProvider({language: langId}, provider));
+		} else {
+			// provider is already managed by the manager but is inactive.
+			manager.providers[providerIdx].active = true;
+		}
+		vscode.window.showInformationMessage('Int2Date: Hover is activated on ' + langId);
+	});
+	context.subscriptions.push(activateHover);
+
+	//
+	// command[int2date.deactiveHover]: deactivate all Int2DateHoverProvider
+	//
+	let deactiveHover = vscode.commands.registerCommand('int2date.deactiveHover', () => {
+		var langIds = [];
+		for (var i=0; i<manager.providers.length; ++i) {
+			let provider = manager.providers[i];
+			if (provider.active) {
+				langIds.push(provider.langId);
+				provider.active = false;
+			}
+		}
+		let msg = langIds.length === 0
+			? 'Int2Date: No Int2Date hover was activated'
+			: 'Int2Date: Deactivated hovers on [' + langIds.join(', ') + ']';
+		vscode.window.showInformationMessage(msg);
+	});
+	context.subscriptions.push(deactiveHover);
+
+	//
+	// command[int2date.listUpActiveHoverList]: list up Int2DateHoverProvider 
+	//
+	let listUpActiveHoverList = vscode.commands.registerCommand('int2date.listUpActiveHoverList', () => {
+		var activeLangIds = [];
+		for (var i=0; i<manager.providers.length; ++i) {
+			let provider = manager.providers[i];
+			if (provider.active) {
+				activeLangIds.push(provider.langId);
+			}
+		}
+		let msg = activeLangIds.length === 0
+			? 'Int2Date: No Int2Date hover is activated'
+			: 'Int2Date: Int2Date hovers are active on [' + activeLangIds.join(', ') + ']';
+		vscode.window.showInformationMessage(msg);
+	});
+	context.subscriptions.push(listUpActiveHoverList);
 }
 
 // -----------------------------------------------------------------------------
-// deactiavter
+// deactivater
 //
 export function deactivate() {}
